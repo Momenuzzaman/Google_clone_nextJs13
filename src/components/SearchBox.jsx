f4f3e11ccb72b25c5bcea24f4e7edfdb5f39b4d7
mdemon420 <mdeamon4@gmail.com>
2023-09-07 01:22:44 +0600

    complete search box components

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const searchTerm = searchParams.get("searchTerm");
+  const [term, setTerm] = useState(searchTerm || "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`/search/web?searchTerm=${term}`);
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex items-center border border-gray-200 rounded-full shadow-lg px-6 py-2 sm:ml-10 mr-5 max-w-3xl flex-grow"
+    >
+      <input
+        type="text"
+        onChange={(e) => setTerm(e.target.value)}
+        value={term}
+        className="w-full focus:outline-none"
+      />
+      <RxCross2
+        onClick={() => setTerm("")}
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+      />
+      <BsFillMicFill
+        className="hidden sm:inline-flex text-4xl text-blue-500 pl-2 border-l-2 border-gray-300 mr-3
+      "
+      />
+      <AiOutlineSearch
+        onClick={handleSubmit}
+        className="hidden sm:inline-flex
+       text-2xl text-blue-500 cursor-pointer"
+      />
+    </form>
+  );
 };
 
 export default SearchBox;
--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -8,7 +8,7 @@ import SearchHeaderOptions from "./SearchHeaderOptions";
 const SearchHeader = () => {
   return (
     <header className="sticky top-0 bg-white">
-      <div className="flex items-center justify-between w-full p-6">
+      <div className="flex items-center justify-between w-full p-6  flex-col sm:flex-row">
         <Link href={"/"}>
           <Image
             width="120"
@@ -16,16 +16,19 @@ const SearchHeader = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
           />
         </Link>
-        <div className="flex-1">
+        <div className="flex-1 mt-2 sm:mt-0">
           <SearchBox />
         </div>
-        <div className="hidden md:inline-flex space-x-2">
+        <div className="inline-flex  space-x-2 mt-2 sm:mt-0">
           <RiSettings3Line className="header-icon " />
           <TbGridDots className="header-icon " />
+          <button className="bg-blue-500 text-white px-6 py-2 rounded-md font-medium hover:brightness-105 hover:shadow-md transition-all ml-2">
+            Sign in
+          </button>
         </div>
-        <button className="bg-blue-500 text-white px-6 py-2 rounded-md font-medium hover:brightness-105 hover:shadow-md transition-all ml-2">
+        {/* <button className="bg-blue-500 text-white px-6 py-2 rounded-md font-medium hover:brightness-105 hover:shadow-md transition-all ml-2">
           Sign in
-        </button>
+        </button> */}
       </div>
       <SearchHeaderOptions />
     </header>
