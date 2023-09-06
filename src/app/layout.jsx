acf00eb81e7045764316833d780ee7549eb8af6f
mdemon420 <mdeamon4@gmail.com>
2023-09-06 18:28:09 +0600

    add footer component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -14,7 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        {/* footer */}
+        <Footer />
+      </body>
     </html>
   );
 }
