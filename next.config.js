b4f427f238584b9de8be6cae5364342311d57a8a
mdemon420 <mdeamon4@gmail.com>
2023-09-06 19:20:30 +0600

    careate search page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn2.downdetector.com"],
+    domains: ["upload.wikimedia.org"],
   },
 };
 
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ export default function Home() {
         <Image
           width="300"
           height="100"
-          src="https://cdn2.downdetector.com/static/uploads/logo/Google-new_19.png"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png"
         />
         <HomeSearch />
       </div>
